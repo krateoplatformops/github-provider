8575814ec06f2e7a6bfe6464bd88af8afa644e52
Luca Sepe <luca.sepe@gmail.com>
2023-02-14 15:24:30 +0100

    fix crd generate

--- apis/generate.go
+++ apis/generate.go
@@ -2,7 +2,7 @@
 // +build generate
 
 // Remove existing CRDs
-//go:generate rm -rf ../chart/crds
+//go:generate rm -rf ../crds
 
 // Generate deepcopy methodsets and CRD manifests
 //go:generate go run -tags generate sigs.k8s.io/controller-tools/cmd/controller-gen object:headerFile=../hack/boilerplate.go.txt paths=./... crd:crdVersions=v1 output:artifacts:config=../crds
